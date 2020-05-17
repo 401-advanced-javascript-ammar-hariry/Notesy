7d573a170a70de39309318ca4b93f2da023fe934
Ammarhr <hariry49@gmail.com>
2020-05-18 01:37:54 +0300

    command line app for notes

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Input = require('./lib/input');
+const Note = require('./lib/note');
+
+const inputObj = new Input;
+
+const objOFNote = new Note(inputObj);
+
+objOFNote.execute(inputObj);
+objOFNote.add(inputObj);
