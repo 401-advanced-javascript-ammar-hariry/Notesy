f2c8cabaf48c47926c900ca75126e06203c84cb1
Ammarhr <hariry49@gmail.com>
2020-05-20 04:55:50 +0300

    CRUD mongodb

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 'use strict';
-
+const mongoose = require('mongoose');
 const Input = require('./lib/input');
 const Note = require('./lib/note');
 
-const inputObj = new Input();
+const DB_URI = 'mongodb://localhost:27017/nosty';
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const inputObj = new Input();
 const objOFNote = new Note(inputObj);
 
-objOFNote.execute(inputObj);
-objOFNote.add(inputObj);
+// objOFNote.execute(inputObj);
+// objOFNote.add(inputObj);
+
+if (inputObj.valid()) {
+  objOFNote.execute(inputObj);
+  objOFNote.add(inputObj).then(mongoose.disconnect);
+} else if (inputObj.list()) {
+  objOFNote.listRender().then(mongoose.disconnect);
+} else if (inputObj.delete()) {
+  objOFNote.deleteNote(inputObj).then(mongoose.disconnect);
+} else {
+  help();
+}
+
+function help() {
+  console.log('error------> you have to input a valid method (--add, -a,--a) or valid note ("string")');
+  process.exit();
+}
