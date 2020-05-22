86bf4beda9703d8b5a443c9ee958d3af7b03f106
Ammarhr <hariry49@gmail.com>
2020-05-22 23:43:31 +0300

    still need

--- __test__/note.test.js
+++ __test__/note.test.js
@@ -5,22 +5,33 @@ const Note = require('../lib/note');
 jest.spyOn(global.console, 'log');
 
 describe('Note Module', () => {
-  it('execute() does its work', () => {
-    const objOFNote = new Note({ payload: 'yes', action: '--a' });
-    objOFNote.execute({ payload: 'yes', action: '--a' });
-    expect(console.log).toHaveBeenCalled();
-  });
+    it('execute() does its work', () => {
+        const objOFNote = new Note({ payload: 'yes', action: '--a' });
+        objOFNote.execute({ payload: 'yes', action: '--a' });
+        expect(console.log).toHaveBeenCalled();
+    });
 
-  it('add() does its work', () => {
-    const objOFNote = new Note({ action: '--a', error: 'ERROR: this is not valid arg' });
-    objOFNote.add({ action: '--a', error: 'ERROR: this is not valid arg' });
-    expect(console.log).toHaveBeenCalled();
-  });
+    it('add() does its work', () => {
+        const objOFNote = new Note({ action: '--a', error: 'ERROR: this is not valid arg' });
+        objOFNote.add({ action: '--a', error: 'ERROR: this is not valid arg' });
+        expect(console.log).toHaveBeenCalled();
+    });
 
-  it('add() does its work', () => {
-    const objOFNote = new Note({ payload: 'yes', action: '--a', note: 'valid note' });
-    objOFNote.add({ payload: 'yes', action: '--a', note: 'valid note' });
-    expect(console.log).toHaveBeenCalled();
-  });
+    it('add() does its work', () => {
+        const objOFNote = new Note({ payload: 'yes', action: '--a', note: 'valid note' });
+        objOFNote.add({ payload: 'yes', action: '--a', note: 'valid note' });
+        expect(console.log).toHaveBeenCalled();
+    });
+    it('render() does it work', () => {
+        const addObj = new Note();
+        addObj.render({ payload: 'yes', action: '--a', note: 'valid note' });
+        expect(console.log).toHaveBeenCalled();
+    });
+    it('render() does it work', () => {
+        const addObj = new Note();
+        addObj.render({ payload: 'yes', action: '--a', note: 'valid note' });
+        addObj.listRender(addObj);
+        expect(console.log).toHaveBeenCalled();
+    });
 
 });
